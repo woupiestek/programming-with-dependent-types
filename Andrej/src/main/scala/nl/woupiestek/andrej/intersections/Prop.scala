84dc3a4766f10bd20b09cadec2fecf510884dfc0
Wouter Stekelenburg <woupiestek@yahoo.com>
2017-04-20 22:08:36 +0200

    Clean up

--- Andrej/src/main/scala/nl/woupiestek/andrej/intersections/Prop.scala
+++ Andrej/src/main/scala/nl/woupiestek/andrej/intersections/Prop.scala
@@ -12,41 +12,39 @@ object Prop {
 
   object False extends Prop
 
-  def overBind(index: Int, inType: InType): Prop = {
+  def overBind(index: Int, inType: InType): Set[Prop] = Set {
     if (freeVars(inType).contains(index)) False else UB(index, inType)
   }
 
-  def underBind(inType: InType, index: Int): Prop = {
+  def underBind(inType: InType, index: Int): Set[Prop] = Set {
     if (freeVars(inType).contains(index)) False else LB(inType, index)
   }
 
+  val fail: Set[Prop] = Set(False)
+
   def exists(props: Set[Prop]): Set[Prop] = {
     val ub = intersection(props.collect { case UB(0, t) => t })
-    val r: Set[Prop] = props.collect {
+    props.flatMap {
       case LB(t, i) if i > 0 => underBind(replace(t, ub), i - 1)
       case UB(i, t) if i > 0 => overBind(i - 1, replace(t, ub))
-    }
-    val p = props.flatMap {
       case LB(t, 0) => leq(t, ub)
       case _ => Set.empty[Prop]
     }
-    p union r
   }
 
-  def leq(x: InType, y: InType): Set[Prop] = {
-    x match {
-      case Var(i) if y == Var(i) => Set.empty
-      case Var(i) if y != Var(i) => Set(overBind(i, y))
-      case Intersection(ts) => ts.map(leq(_, y)).find(s => !s.contains(False)).getOrElse(Set(False))
-      case Forall(t) => exists(leq(t, insert(y)))
-      case Arrow(a, b) => y match {
-        case Var(i) if !freeVars(x).contains(i) => Set(underBind(x, i))
-        case Intersection(ts) => ts.flatMap(leq(x, _))
-        case Forall(t) => leq(insert(x), t)
-        case Arrow(c, d) => leq(c, a) union leq(b, d)
-        case _ => Set.empty[Prop]
-      }
+  def leq(x: InType, y: InType): Set[Prop] = x match {
+    case Var(i) if y == Var(i) => Set.empty
+    case Var(i) if y != Var(i) => overBind(i, y)
+    case Intersection(ts) => ts.map(leq(_, y)).find(s => !s.contains(False)).getOrElse(Set(False))
+    case Forall(t) => exists(leq(t, insert(y)))
+    case Arrow(a, b) => y match {
+      case Var(i) if !freeVars(x).contains(i) => underBind(x, i)
+      case Intersection(ts) => ts.flatMap(leq(x, _))
+      case Forall(t) => leq(insert(x), t)
+      case Arrow(c, d) => leq(c, a) union leq(b, d)
+      case _ => fail
     }
+    case _ => fail
   }
 
   def combine(x: InType, y: InType): InType = x match {
