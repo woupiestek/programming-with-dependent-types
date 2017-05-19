29d757260372e8d1f82eb1025c850a66d0d74cbb
Wouter Stekelenburg <woupiestek@yahoo.com>
2017-05-19 22:38:34 +0200

    Improve repl

--- Andrej/src/main/scala/nl/woupiestek/andrej/interpreter/Repl.scala
+++ Andrej/src/main/scala/nl/woupiestek/andrej/interpreter/Repl.scala
@@ -1,17 +1,17 @@
 package nl.woupiestek.andrej.interpreter
 
-import nl.woupiestek.andrej.typeclasses.Monad
+import nl.woupiestek.andrej.free.MonadRec
 import nl.woupiestek.andrej.typeclasses.Monad._
-import scala.language.higherKinds
 
 import scala.io.StdIn
+import scala.language.higherKinds
+import scalaz.{ -\/, \/, \/- }
 
 trait REPL[IO[_]] {
   def write(message: String): IO[Unit]
 
   def read(prompt: String): IO[String]
 
-  def doWhile[T](condition: T => IO[Boolean], program: T => IO[T], init: T): IO[T]
 }
 
 object REPL {
@@ -20,9 +20,6 @@ object REPL {
 
   def read[IO[_]](prompt: String)(implicit IO: REPL[IO]): IO[String] = IO.read(prompt)
 
-  implicit class Looper[IO[_], T](t: T)(implicit IO: REPL[IO] with Monad[IO]) {
-    def doWhile(c: (T) => IO[Boolean], p: (T) => IO[T]): IO[T] = IO.doWhile(c, p, t)
-  }
 }
 
 object TrivialREPL extends App {
@@ -31,34 +28,36 @@ object TrivialREPL extends App {
 
   type Program[T] = Unit => T
 
-  implicit val console = new REPL[Program] with Monad[Program] {
+  implicit val console = new REPL[Program] with MonadRec[Program] {
     override def write(message: String): Program[Unit] = unit(println(message))
 
     override def read(prompt: String): Program[String] = unit(StdIn.readLine(prompt))
 
-    override def doWhile[T](c: (T) => Program[Boolean], p: (T) => Program[T], t: T): Program[T] = {
-      def fixand(u: T): T = if (c(u).apply(())) fixand(p(u).apply(())) else u
-
-      _ => fixand(t)
-    }
-
     override def unit[A](a: A): Program[A] = _ => a
 
     override def bind[A, B](fa: Program[A])(f: (A) => Program[B]): Program[B] = f(fa.apply(()))
+
+    override def tailRec[A, B](f: (A) => Program[\/[A, B]]): (A) => Program[B] = {
+      def run: A => B = a => f(a)(()) match {
+        case -\/(a0) => run(a0)
+        case \/-(b) => b
+      }
+
+      a => _ => run(a)
+    }
   }
 
   val program: Program[Unit] = for {
     _ <- write("Welcome to the REPL")
     x <- read("Q:")
-    _ <- x.doWhile(
-      input => console.unit(!input.equalsIgnoreCase("exit")),
-      input => for {
+    _ <- console.tailRec[String, Unit](input =>
+      if (input.equalsIgnoreCase("exit")) write("bye bye").map(\/-(_))
+      else for {
         _ <- write(input)
         y <- read("Q:")
-      } yield y)
-    _ <- write("bye bye")
+      } yield -\/(y))(x)
   } yield ()
 
-  program.apply(())
+  program(())
 
 }
